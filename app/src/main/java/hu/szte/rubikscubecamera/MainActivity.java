58a14ae4ea2363486e98fff10329970164b44d22
P6atrk <patrik.peto43@gmail.com>
2023-03-26 20:57:47 +0200

    added toolbar to MainActivity

--- app/src/main/java/hu/szte/rubikscubecamera/MainActivity.java
+++ app/src/main/java/hu/szte/rubikscubecamera/MainActivity.java
@@ -1,16 +1,22 @@
 package hu.szte.rubikscubecamera;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import hu.szte.rubikscubecamera.databinding.ActivityMainBinding;
+import hu.szte.rubikscubecamera.ui.guide.GuideActivity;
+import hu.szte.rubikscubecamera.ui.options.OptionsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -34,4 +43,29 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_options:
+                Intent optionsIntent = new Intent(MainActivity.this, OptionsActivity.class);
+                startActivity(optionsIntent);
+                return true;
+            case R.id.navigation_guide:
+                Intent guideIntent = new Intent(MainActivity.this, GuideActivity.class);
+                startActivity(guideIntent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
---
+++ app/src/main/java/hu/szte/rubikscubecamera/ui/guide/GuideActivity.java
@@ -0,0 +1,16 @@
+package hu.szte.rubikscubecamera.ui.guide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hu.szte.rubikscubecamera.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
