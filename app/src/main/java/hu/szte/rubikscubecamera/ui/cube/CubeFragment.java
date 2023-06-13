6b8ee3c371c504d789805286bb51ce14379536a8
P6atrk <35540235+P6atrk@users.noreply.github.com>
2023-06-13 16:15:19 +0200

    Feature/11 intergrate kociemba (#14)
    * added kociemba and implementation
    
    * added tests for KociembaImpl
    
    * ignore .idea files

--- app/src/main/java/hu/szte/rubikscubecamera/ui/cube/CubeFragment.java
+++ app/src/main/java/hu/szte/rubikscubecamera/ui/cube/CubeFragment.java
@@ -5,30 +5,35 @@ import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import hu.szte.rubikscubecamera.databinding.FragmentCubeBinding;
 
 public class CubeFragment extends Fragment {
-
     private FragmentCubeBinding binding;
+
+    private CubeViewModel cubeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        CubeViewModel cubeViewModel =
-                new ViewModelProvider(this).get(CubeViewModel.class);
+        cubeViewModel = new ViewModelProvider(this).get(CubeViewModel.class);
 
         binding = FragmentCubeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textCube;
-        cubeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textCube = binding.textCube;
+        final Button buttonSolveCubeRandom = binding.buttonSolveRandomCube;
+
+        buttonSolveCubeRandom.setOnClickListener(a -> cubeViewModel.solveRandomCube());
+
+        cubeViewModel.getTextCube().observe(getViewLifecycleOwner(), textCube::setText);
+
         return root;
     }
 
